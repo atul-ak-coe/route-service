9fd8e420d7714e959c47b0e49ffa08e23d0d45f5
Atul Kumar <atul.ak@gmail.com>
2023-03-13 18:52:52 +0530

    Revert "Initial commit"
    This reverts commit bccec289604ee7f1413adbdaf07a6a9103639836.

--- src/main/java/com/fleet/management/route/respository/RouteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fleet.management.route.respository;
-
-import com.fleet.management.route.model.RouteDetail;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RouteRepository extends ReactiveCrudRepository<RouteDetail, Long> {
-}
